b578a24e6e20c8dcf1098fe50e5f57f85ba29dda
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-08-03 12:54:25 +0200

    Adding auth option.

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -2,13 +2,25 @@
 SOURCE_DIR=$(dirname $0)
 ZABBIX_DIR=/etc/zabbix
 
-ELASTIC_SERVER=${1:-localhost}
-ELASTIC_PORT=${2:-9022}
-ELASTIC_METHOD=${3:-http}
-ELASTIC_URL="${ELASTIC_METHOD}://${ELASTIC_SERVER}:${ELASTIC_PORT}"
+ELASTIC_URL =${1:-'http://localhost:9022'}
+ELASTIC_USER=${4:-monitor}
+ELASTIC_PASS=${5:-m0n1t0r}
 
 mkdir -p ${ZABBIX_DIR}/scripts/agentd/elasix
-cp ${SOURCE_DIR}/elasix/elasix.conf.example ${ZABBIX_DIR}/scripts/agentd/elasix/elasix.conf
-cp ${SOURCE_DIR}/elasix/elasix.sh ${ZABBIX_DIR}/scripts/agentd/elasix/
-cp ${SOURCE_DIR}/elasix/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/elasix.conf
-sed -i "s|ELASTIC_URL=.*|ELASTIC_URL=\"${ELASTIC_URL}\"|g" ${ZABBIX_DIR}/scripts/agentd/elasix/elasix.conf
+
+SCRIPT_CONFIG=${ZABBIX_DIR}/scripts/agentd/elasix/elasix.conf
+if [[ -f ${SCRIPT_CONFIG} ]]; then
+    SCRIPT_CONFIG=${ZABBIX_DIR}/scripts/agentd/elasix/elasix.conf.new
+fi
+
+cp -rpv ${SOURCE_DIR}/elasix/elasix.conf.example ${SCRIPT_CONFIG}/scripts/agentd/elasix/elasix.conf
+cp -rpv ${SOURCE_DIR}/elasix/elasix.sh           ${ZABBIX_DIR}/scripts/agentd/elasix/
+cp -rpv ${SOURCE_DIR}/elasix/zabbix_agentd.conf  ${ZABBIX_DIR}/zabbix_agentd.d/elasix.conf
+
+regex_array[0]="s|ELASTIC_URL=.*|ELASTIC_URL=\"${ELASTIC_URL}\"|g"
+regex_array[1]="s|ELASTIC_USER=.*|ELASTIC_USER=\"${ELASTIC_USER}\"|g"
+regex_array[2]="s|ELASTIC_PASS=.*|ELASTIC_PASS=\"${ELASTIC_PASS}\"|g"
+for index in ${!regex_array[*]}; do
+    sed -i "${regex_array[${index}]}" ${ZABBIX_SCRIPT_CONFIG}
+done
+
